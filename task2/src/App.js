e1224172a4bc8cad0eb4a31b5d7bfecf46bf2df5
Sumit tiwari <72980463+st0107@users.noreply.github.com>
2022-05-28 17:51:25 +0530

    added react app

---
+++ task2/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import ToDoList from './Components/ToDoList'
+
+function App() {
+  return (
+    
+    <div>
+      <div className='header'>
+        <h1>ToDo List</h1>
+      </div>
+      <div className="">
+
+        
+        <ToDoList></ToDoList>
+      </div>
+    </div>
+  );
+}
+
+export default App;
