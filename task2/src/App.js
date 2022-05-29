e6f0d9157a07fff1c38924f00dfefa007f4f923a
Sumit tiwari <72980463+st0107@users.noreply.github.com>
2022-05-29 15:36:06 +0530

    task2 done

--- task2/src/App.js
+++ task2/src/App.js
@@ -1,20 +1,43 @@
 import './App.css';
-import ToDoList from './Components/ToDoList'
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    
-    <div>
-      <div className='header'>
-        <h1>ToDo List</h1>
-      </div>
-      <div className="">
+    const [ users, setUser ] = useState();
+    const fetchData = async () => {
+            fetch("https://reqres.in/api/users?page=1")
+              .then(response => {
+                return response.json()
+              })
+              .then(data => {
+                setUser(data)
+                console.log(users)
+              }).catch((error)=>{
+                  console.log(error)
+              })
+          }
 
-        
-        <ToDoList></ToDoList>
-      </div>
-    </div>
-  );
+    return (
+
+        <div className = 'App' >
+            <div className = 'header' >
+            <h1 > ABC </h1> 
+            <button onClick = {fetchData} > Get Users </button> 
+            </div >
+            <div className = "Users" >
+            <h2 > Users: </h2>
+            <ul className='user_list'>
+                {users && users.data.map(({ id, first_name, last_name, email, avatar })=>(
+                    <li className='user' key = {id}>
+                        <img src= {avatar} alt={'image of '+first_name}/> <br/>
+                        name: { first_name + ' ' + last_name } <br/>
+                        email: {email}<br/>
+                    </li>
+                ))}
+                
+            </ul>
+
+            </div> 
+        </div>
+    );
 }
-
 export default App;
